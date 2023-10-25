8045a85b155c6eab76c595cc1244233393c7fcdb
vladimir.pelikan <vladimir.pelikan@globallogic.com>
2023-10-25 13:55:35 +0200

    maintenance of testcases for  user endpoint and store endpoint and tests added

--- endpoint.py
+++ endpoint.py
@@ -1,6 +1,7 @@
 import requests
 
 User_ENDPOINT = "https://petstore.swagger.io/v2/user"
+Store_ENDPOINT = "https://petstore.swagger.io/v2/store"
 
 def create_user(payload):
     return requests.post(User_ENDPOINT, json=payload)
@@ -14,10 +15,39 @@ def get_user(user_name):
 def delete_user(user_name):
     return requests.delete(User_ENDPOINT + f"/{user_name}")
 
+def login_user(user_name, password):
+    return requests.get(User_ENDPOINT + "/login")
+
+def create_list(payload):
+    return requests.post(User_ENDPOINT + "/createWithList", json=payload)
+
+def create_list_users():
+    dict = [{
+        "id": 1,
+        "username": "user1",
+        "firstName": 'Ferko',
+        "lastName": "Mrkvicka",
+        "email": "ferko.mrkvicka@gmail.com",
+        "password": "1234",
+        "phone": "421949888555",
+        "userStatus": 0
+    },
+        {
+            "id": 2,
+            "username": "user2",
+            "firstName": 'Fer',
+            "lastName": "Mrk",
+            "email": "fer.mrk@gmail.com",
+            "password": "4321",
+            "phone": "421949888333",
+            "userStatus": 0
+        }
+    ]
+    return dict
 def new_user_payload():
-    user_name = "user1"
+    user_name = "user3"
     return {
-        "id": 0,
+        "id": 1,
         "username": user_name,
         "firstName": 'Ferko',
         "lastName": "Mrkvicka",
@@ -26,3 +56,27 @@ def new_user_payload():
         "phone": "421949888555",
         "userStatus": 0,
     }
+
+# store endpoint
+
+def create_order():
+    return {
+  "id": 1,
+  "petId": 1,
+  "quantity": 2,
+  "shipDate": "2023-10-25T09:39:01.330Z",
+  "status": "placed",
+  "complete": True
+}
+
+def store_order(payload):
+    return requests.post(Store_ENDPOINT + "/order", json=payload)
+
+def get_order(orderId):
+    return requests.get(Store_ENDPOINT + f"/order/{orderId}")
+
+def delete_order(orderId):
+    return requests.delete(Store_ENDPOINT + f"/order/{orderId}")
+
+def get_inventory():
+    return requests.get(Store_ENDPOINT + "/inventory")
--- test_steps.py
+++ test_steps.py
@@ -1,6 +1,5 @@
 import requests #to request https endpoint
 import endpoint as ENDPOINT
-import uuid # unigue identifier
 
 
 def test_can_call_endpoint():
@@ -13,7 +12,6 @@ def test_can_create_user():
     assert create_user_response.status_code == 200
 
     data = create_user_response.json()
-    print(data)
 
     status = data["code"]
     get_response = create_user_response.status_code
@@ -25,7 +23,6 @@ def test_can_create_user():
     get_user_response = get_user_response.json()
     assert get_user_response["firstName"] == payload["firstName"]
     assert get_user_response["username"] == payload["username"]
-    print(get_user_response)
 
 def test_can_update_user():
     payload = ENDPOINT.new_user_payload()
@@ -57,10 +54,59 @@ def test_can_delete_user():
     create_user_response = ENDPOINT.create_user(payload)
     assert create_user_response.status_code == 200
     username = payload["username"]
-    print(username)
 
     delete_user_response = ENDPOINT.delete_user(username)
     assert delete_user_response.status_code == 200
 
     get_user_response = ENDPOINT.get_user(username)
     assert get_user_response.status_code == 404
+
+def test_can_login_user():
+    payload = ENDPOINT.new_user_payload()
+    create_user_response = ENDPOINT.create_user(payload)
+    assert create_user_response.status_code == 200
+    data = create_user_response.json()
+
+    status = data["code"]
+    get_response = create_user_response.status_code
+    assert get_response == status
+
+    get_user_response = ENDPOINT.login_user(user_name=payload["username"], password=payload["password"])
+    assert get_user_response.status_code == 200
+
+def test_can_create_list():
+    payload = ENDPOINT.create_list_users()
+    create_user_response = ENDPOINT.create_list(payload)
+    assert create_user_response.status_code == 200
+
+def test_can_order():
+    payload = ENDPOINT.create_order()
+    create_order_response = ENDPOINT.store_order(payload)
+    assert create_order_response.status_code == 200
+
+    get_order_data = create_order_response.json()
+    assert get_order_data["petId"] == payload["petId"]
+    assert get_order_data["quantity"] == payload["quantity"]
+
+    orderId = payload["id"]
+    get_order_response = ENDPOINT.get_order(orderId)
+
+    get_order_response = get_order_response.json()
+    assert get_order_response["petId"] == payload["petId"]
+    assert get_order_response["quantity"] == payload["quantity"]
+
+def test_can_delete_order():
+    payload = ENDPOINT.create_order()
+    create_order_response = ENDPOINT.store_order(payload)
+    assert create_order_response.status_code == 200
+    id = payload["id"]
+
+    delete_order_response = ENDPOINT.delete_user(id)
+    assert delete_order_response.status_code == 200
+
+    get_order_response = ENDPOINT.get_order(id)
+    assert get_order_response.status_code == 404
+
+def test_can_check_inventory():
+    get_inventory_response = ENDPOINT.get_inventory()
+    assert get_inventory_response.status_code == 200
